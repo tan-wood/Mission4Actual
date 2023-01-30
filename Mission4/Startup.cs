0b427e8d697dfc4f1eb466d5104842ad65616e15
tannerwoodrum <tannerwoodrum@TANNERWOODR8C05>
2023-01-30 11:02:26 -0700

    Added MVC capabilities and also split the grade calculator up from the index page

--- Mission4/Startup.cs
+++ Mission4/Startup.cs
@@ -10,18 +10,33 @@ using System.Threading.Tasks;
 
 namespace Mission4
 {
+    // If we go to right click on the project>properties>debug>change the environment to production to see what the errors look like
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDefaultFiles();
+            if (env.IsEnvironment("Development"))
+            {
+                app.UseDeveloperExceptionPage(); //this will help us developers be able to see ALL THE ERRORS
+
+            }
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                    );
+            });
 
             app.UseStaticFiles();
         }
